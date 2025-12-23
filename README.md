8eb1c206cea13ae6f121efeac507f84890c5fcdc
Piotr Wierzbicki <wi3rzbicki.piotr@gmail.com>
2025-12-23 21:48:33 +0100

    Skakanie

--- README.md
+++ README.md
@@ -4,3 +4,15 @@ Jak bedziecie chceli to kompilowac to musicie uzyc takiej komedy
 i do tego musicie miec zainstalowaną ta biblioteke raylib.h na ubuntu robi się to tak 
 `sudo apt update`
 `sudo apt install libraylib-dev`
+
+Komendy do gita: 
+
+`git status` - sprawdzanie zmian 
+
+`git commit -a -m "wiadomosc"` - commituje zmiany na lokalnego brancha
+
+`git push` - wrzuca zmiany na server 
+
+`git pull` - zciąga zmiany na lokalne urządzenie 
+
+Pozycja (0, 0) z tego co rozumiem jest w lewym górnym rogu okna
--- main_file.cpp
+++ main_file.cpp
@@ -1,79 +1,88 @@
 
-//biblioteki
-#include <iostream> 
+// biblioteki
+#include <iostream>
 #include "raylib.h"
 
-//wartosci dla okna
+// wartosci dla okna
 #define wysokosc_okna 720
 #define szerokosc_okna 1280
-#define nazwa_gry_wyswietlana_na_oknie "Nazwa Gry" //to zmienimy jak ustalimy fabule
+#define nazwa_gry_wyswietlana_na_oknie "Nazwa Gry" // to zmienimy jak ustalimy fabule
 #define ilosc_fps 60
 
-//uzywam do skalowania grafiki gracza by byla mniejsza 
+// uzywam do skalowania grafiki gracza by byla mniejsza
 #define skalowanie_obrazu 0.25f
 
-//porusznie sie
+// porusznie sie
 #define prendkosc 300.0f
-void skakanie();
+#define wysokosc_skoku 60
+
+// prototypy funkcji
+float skakanie(float polozenie, int liczba_klatek, float dt);
 
 using namespace std;
 
 int main()
 {
-    InitWindow(szerokosc_okna, wysokosc_okna, nazwa_gry_wyswietlana_na_oknie); //inicjuje otwarcie okna o podanych wymiarach
-    SetTargetFPS(ilosc_fps); // ustala ilosc docelowa fps w oknie 
+    InitWindow(szerokosc_okna, wysokosc_okna, nazwa_gry_wyswietlana_na_oknie); // inicjuje otwarcie okna o podanych wymiarach
+    SetTargetFPS(ilosc_fps);                                                   // ustala ilosc docelowa fps w oknie
 
-    Texture2D tekstura_gracza = LoadTexture("assets/gracz_1.png"); //zapisywanie tekstury gracza z katalogu assets i pliku gracz_1
-    
+    Texture2D tekstura_gracza = LoadTexture("assets/gracz_1.png"); // zapisywanie tekstury gracza z katalogu assets i pliku gracz_1
 
     // wartości dla postaci
     float polozenie_gracza_x = 20;
-    float polozenie_gracza_y = (GetScreenHeight() - tekstura_gracza.height * skalowanie_obrazu) / 2.0f; // ustawianie go na srodku 
-    
-    Vector2 polozenie_gracza = {polozenie_gracza_x, polozenie_gracza_y}; //inicjowanie vektora 2 wymiarowego 
+    float polozenie_gracza_y = (GetScreenHeight() - tekstura_gracza.height * skalowanie_obrazu) / 2.0f; // ustawianie go na srodku
+
+    Vector2 polozenie_gracza = {polozenie_gracza_x, polozenie_gracza_y}; // inicjowanie vektora 2 wymiarowego
+
+    int klatka_skoku = -1;
 
     while (!WindowShouldClose()) // utrzymuje okno otwarte i wykonuje polecenie wymagane przy operacji na oknach
     {
-        //poruszanie sie
+        // poruszanie sie
         float dt = GetFrameTime();
 
-        // RUCH: góra/dół
-        if (IsKeyDown(KEY_W))
+        if (klatka_skoku >= wysokosc_skoku)
         {
-            polozenie_gracza.y -= prendkosc * dt;
-        }  
-        if (IsKeyDown(KEY_S))
-        {
-            polozenie_gracza.y += prendkosc * dt;
-        }
-        // OGRANICZENIE DO OKNA
-        float playerH = tekstura_gracza.height * skalowanie_obrazu;
-        if (polozenie_gracza.y < 0)
-        {
-            polozenie_gracza.y = 0;
-        }
-        if (polozenie_gracza.y > GetScreenHeight() - playerH)
-        {
-          polozenie_gracza.y = GetScreenHeight() - playerH;  
+            klatka_skoku = -1;
         }
 
-        //rysowanie grafiki gracza 
+        if (IsKeyPressed(KEY_SPACE) && klatka_skoku == -1)
+        {
+            klatka_skoku = 0;
+        }
+
+        if (klatka_skoku != -1) {
+            polozenie_gracza.y = skakanie(polozenie_gracza.y, klatka_skoku, dt);
+            klatka_skoku ++;
+        }
+
+        // rysowanie grafiki gracza
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
-        DrawTextureEx(tekstura_gracza, polozenie_gracza, 0.0f, skalowanie_obrazu, WHITE); //rysowanie gracza w zdefiniowanej skali
+        DrawTextureEx(tekstura_gracza, polozenie_gracza, 0.0f, skalowanie_obrazu, WHITE); // rysowanie gracza w zdefiniowanej skali
 
         EndDrawing();
     }
 
-    //zwalnianie pamieci 
+    // zwalnianie pamieci
     UnloadTexture(tekstura_gracza);
     CloseWindow();
 
     return 0;
 }
 
-void skakanie()
+float skakanie(float polozenie, int liczba_klatek, float dt)
 {
-    return;
+    float polozenie_koncowe = polozenie;
+
+    if (liczba_klatek <= (wysokosc_skoku/2))
+    {
+        polozenie_koncowe -= prendkosc * dt;
+    }
+    else if (liczba_klatek > (wysokosc_skoku/2))
+    {
+        polozenie_koncowe += prendkosc * dt;
+    }
+    return polozenie_koncowe;
 }
