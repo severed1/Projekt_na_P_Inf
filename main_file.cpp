040c6fbed80ea38f1bc2b752dc53ea172fe13cef
Piotr Wierzbicki <wi3rzbicki.piotr@gmail.com>
2025-12-28 17:35:08 +0100

    poprawa skoku i przeskalowanie do 1280 * 720

--- main_file.cpp
+++ main_file.cpp
@@ -4,20 +4,22 @@
 #include "raylib.h"
 
 // wartosci dla okna
-#define wysokosc_okna 180
-#define szerokosc_okna 480
+#define wysokosc_okna 720                          // 180
+#define szerokosc_okna 1280                        // 480
 #define nazwa_gry_wyswietlana_na_oknie "Nazwa Gry" // to zmienimy jak ustalimy fabule
-#define ilosc_fps 60
+#define ilosc_fps 120
 
 // uzywam do skalowania grafiki gracza by byla mniejsza
-#define skalowanie_obrazu 0.1f
+#define skalowanie_obrazu_gracza 0.2f // 0.1f
+#define skalowanie_borazu_tla 5.0f
 
 // porusznie sie
 #define prendkosc 300.0f
-#define wysokosc_skoku 60
+#define dlugosc_skoku 20 * 3
+#define wysokosc_skoku 60.0f * 3
 
 // prototypy funkcji
-float skakanie(float polozenie, int liczba_klatek, float dt);
+float skakanie(float ziemiaY, int klatka, int dlugoscSkoku, float jumpHeight);
 
 using namespace std;
 
@@ -30,7 +32,8 @@ int main()
 
     // wartości dla postaci
     float polozenie_gracza_x = 20;
-    float polozenie_gracza_y = (GetScreenHeight() - tekstura_gracza.height * skalowanie_obrazu) / 2.0f; // ustawianie go na srodku
+    float polozenie_gracza_y = GetScreenHeight() - tekstura_gracza.height * skalowanie_obrazu_gracza; // ustawianie go na srodku
+    float ziemiaY = polozenie_gracza_y;
 
     Vector2 polozenie_gracza = {polozenie_gracza_x, polozenie_gracza_y}; // inicjowanie vektora 2 wymiarowego
 
@@ -39,6 +42,7 @@ int main()
     // tło
     Texture2D background = LoadTexture("assets/background/background.png"); // wczytywanie tekstury tła
 
+    float szerokosc_tla_przeskalowana = background.width * skalowanie_borazu_tla;
     float scrollingBack = 0.0f;
     float scrollSpeed = 150.00f; // predkosc przesuwania się tła w pikselach na sekunde
 
@@ -47,13 +51,13 @@ int main()
         // poruszanie sie
         float dt = GetFrameTime();
 
-        scrollingBack -= scrollSpeed * GetFrameTime(); // aktualizacja pozycji tła
-        if (scrollingBack <= -background.width)        // resetowanie pozycji gry tło wyjdzie poza ekran
+        scrollingBack -= scrollSpeed * dt; // aktualizacja pozycji tła
+        if (scrollingBack <= -szerokosc_tla_przeskalowana)       //* skalowanie_borazu_tla) // resetowanie pozycji gry tło wyjdzie poza ekran
         {
-            scrollingBack = 0;
+            scrollingBack += szerokosc_tla_przeskalowana;
         }
 
-        if (klatka_skoku >= wysokosc_skoku)
+        if (klatka_skoku >= dlugosc_skoku)
         {
             klatka_skoku = -1;
         }
@@ -65,7 +69,7 @@ int main()
 
         if (klatka_skoku != -1)
         {
-            polozenie_gracza.y = skakanie(polozenie_gracza.y, klatka_skoku, dt);
+            polozenie_gracza.y = skakanie(ziemiaY, klatka_skoku, dlugosc_skoku, wysokosc_skoku); // 60px wysokości
             klatka_skoku++;
         }
 
@@ -75,33 +79,31 @@ int main()
 
         // rysownanie tła w dwóch kopiach by płynnie tło cały czas się wyświetlało bez luk
         // pierwsza kopia tła
-        DrawTextureEx(background, {scrollingBack, 0}, 0.0f, 1.0f, WHITE);
+        // DrawTextureEx(background, {scrollingBack, 0}, 0.0f, skalowanie_borazu_tla, WHITE);
         // druga kopia doklejona dokładnie za pierwszą
-        DrawTextureEx(background, {scrollingBack + background.width, 0}, 0.0f, 1.0f, WHITE);
+        // DrawTextureEx(background, {scrollingBack + background.width, 0}, 0.0f, skalowanie_borazu_tla, WHITE);
 
-        DrawTextureEx(tekstura_gracza, polozenie_gracza, 0.0f, skalowanie_obrazu, WHITE); // rysowanie gracza w zdefiniowanej skali
+        for (float x = scrollingBack; x < szerokosc_okna; x += szerokosc_tla_przeskalowana)
+        {
+            DrawTextureEx(background, {x, 0}, 0.0f, skalowanie_borazu_tla, WHITE);
+        }
+
+        DrawTextureEx(tekstura_gracza, polozenie_gracza, 0.0f, skalowanie_obrazu_gracza, WHITE); // rysowanie gracza w zdefiniowanej skali
 
         EndDrawing();
     }
 
     // zwalnianie pamieci
+    UnloadTexture(background);
     UnloadTexture(tekstura_gracza);
     CloseWindow();
 
     return 0;
 }
 
-float skakanie(float polozenie, int liczba_klatek, float dt)
+float skakanie(float ziemiaY, int klatka, int dlugoscSkoku, float jumpHeight)
 {
-    float polozenie_koncowe = polozenie;
-
-    if (liczba_klatek <= (wysokosc_skoku / 2))
-    {
-        polozenie_koncowe -= prendkosc * dt;
-    }
-    else if (liczba_klatek > (wysokosc_skoku / 2))
-    {
-        polozenie_koncowe += prendkosc * dt;
-    }
-    return polozenie_koncowe;
+    float t = (float)klatka / (float)dlugoscSkoku;
+    float parabola = 4.0f * t * (1.0f - t);
+    return ziemiaY - jumpHeight * parabola;
 }
